405b4355ca3ca25acc831155afa3cce7e4d8a302
Froyder <homelik@mail.ru>
2021-05-06 18:00:42 +0300

    now it closes fragment and keyboard after adding new note

--- app/src/main/java/com/example/mynotebook/AddNoteFragment.java
+++ app/src/main/java/com/example/mynotebook/AddNoteFragment.java
@@ -1,19 +1,20 @@
 package com.example.mynotebook;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class AddNoteFragment extends Fragment {
 
@@ -43,7 +44,7 @@ public class AddNoteFragment extends Fragment {
         addButton.setOnClickListener(addButtonListener);
         tV = rootView.findViewById(R.id.add_note_name_tV);
         addName = rootView.findViewById(R.id.add_note_name_eT);
-        addText= rootView.findViewById(R.id.add_note_text_eT);
+        addText = rootView.findViewById(R.id.add_note_text_eT);
         model = new ViewModelProvider(requireActivity()).get(MyViewModel.class);
         return rootView;
     }
@@ -51,6 +52,14 @@ public class AddNoteFragment extends Fragment {
     View.OnClickListener addButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
+            Activity activity = getActivity();
+            InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            try {
+                inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+            } catch (Exception e) {
+            }
+
             model.addNote(addName.getText().toString(), addText.getText().toString());
             fM.beginTransaction()
                     .replace(R.id.container, new ListFragment())
